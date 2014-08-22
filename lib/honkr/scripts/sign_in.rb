744a672def1c420eaf9aa8b908ae6eff28558ae3
Catherine O'Neill <cathzozo@gmail.com>
2014-08-21 20:18:53 -0500

    passed sign-in tests

--- lib/honkr/scripts/sign_in.rb
+++ lib/honkr/scripts/sign_in.rb
@@ -2,7 +2,20 @@ module Honkr
   class SignIn
 
     def self.run(params)
-      # TODO
+      user = Honkr.db.get_user_by_username(params[:username])
+      return {:success? => false, :error => "NO SUCH USER"} if !user
+
+      if !user.has_password?(params[:password])
+        return {:success? => false, :error => :invalid_password}
+      end
+
+      session_id = Honkr.db.create_session(user_id: user.id)
+      
+      {
+        :success? => true,
+        :session_id => session_id #user.id
+      }    
+        
     end
 
   end
