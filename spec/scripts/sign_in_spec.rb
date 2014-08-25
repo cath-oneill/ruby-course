a61e2ac08cb668b6bfde2d58055a35cf14dd49e7
Catherine O'Neill <cathzozo@gmail.com>
2014-08-25 15:25:48 -0500

    let(:script) line in sign_in_spec

--- spec/scripts/sign_in_spec.rb
+++ spec/scripts/sign_in_spec.rb
@@ -1,52 +1,42 @@
 require 'spec_helper'
 
 describe DoubleDog::SignIn do
+  let(:script) {DoubleDog::SignIn}
+  
   describe 'validation' do
     it "requires a non-nil username" do
-      script = DoubleDog::SignIn
       result = script.run(username: nil)
-
       expect(result[:success?]).to eq(false)
       expect(result[:error]).to eq(:nil_username)
     end
 
     it "requires a non-blank username" do
-      script = DoubleDog::SignIn
       result = script.run(username: '')
-
       expect(result[:success?]).to eq(false)
       expect(result[:error]).to eq(:blank_username)
     end
 
     it "it requires a non-nil password" do
-      script = DoubleDog::SignIn
       result = script.run(username: 'bob_man', password: nil)
-
       expect(result[:success?]).to eq(false)
       expect(result[:error]).to eq(:nil_password)
     end
 
     it "it requires a non-blank password" do
-      script = DoubleDog::SignIn
       result = script.run(username: 'malice_alice', password: '')
-
       expect(result[:success?]).to eq(false)
       expect(result[:error]).to eq(:blank_password)
     end
 
     it "requires the username to exist" do
-      script = DoubleDog::SignIn
       result = script.run(:username => "doesn't exist", :password => "doesn't exist")
-
       expect(result[:success?]).to eq(false)
       expect(result[:error]).to eq(:no_such_user)
     end
 
     it "requires the password to match the username" do
       user = DoubleDog.db.create_user(username: 'sally', password: 'brownie')
-      script = DoubleDog::SignIn
       result = script.run(:username => user.username, :password => 'pillsbury')
-
       expect(result[:success?]).to eq(false)
       expect(result[:error]).to eq(:invalid_password)
     end
@@ -54,22 +44,17 @@ describe DoubleDog::SignIn do
 
   it "creates a sessions for the user" do
     user = DoubleDog.db.create_user(username: 'alice', password: 'beer')
-    script = DoubleDog::SignIn
     result = script.run(:username => user.username, password: 'beer')
-
     expect(result[:success?]).to eq(true)
 
     session_id = result[:session_id]
-
     expect(user.username).to eq('alice')
     expect(session_id).to_not be_nil
   end
 
   it "retrieves a user from the created session" do
     user = DoubleDog.db.create_user(username: 'bob', password: 'pass12')
-    script = DoubleDog::SignIn
     result = script.run(:username => user.username, password: 'pass12')
-
     expect(result[:success?]).to eq(true)
 
     user = result[:user]
