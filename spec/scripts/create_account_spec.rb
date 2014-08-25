fcec08f08fdedec881ba01ba1ada0a93ece6b7c6
Catherine O'Neill <cathzozo@gmail.com>
2014-08-25 15:16:52 -0500

    implemented helper method in remaining locations

--- spec/scripts/create_account_spec.rb
+++ spec/scripts/create_account_spec.rb
@@ -2,51 +2,38 @@ require 'spec_helper'
 
 describe DoubleDog::CreateAccount do
   let(:script) {DoubleDog::CreateAccount}
-  let(:successful_login) do 
-    expect(script).to receive(:admin_session?).and_return(true)
-  end
-  let(:unsuccessful_login) do
-    expect(script).to receive(:admin_session?).and_return(false)
-  end
 
   describe 'Validation' do
     it "requires the creator to be an admin" do
-      unsuccessful_login = script.run(:session_id => 'nope', :username => 'a', :password => 'b')
-      expect(unsuccessful_login[:success?]).to eq false
-      expect(unsuccessful_login[:error]).to eq :not_admin
+      admin(script, false)
+      result = script.run(:session_id => 'nope', :username => 'a', :password => 'b')
+      expect(result[:success?]).to eq false
+      expect(result[:error]).to eq :not_admin
     end
 
     it "requires a username" do
-      script = DoubleDog::CreateAccount
-      expect(script).to receive(:admin_session?).and_return(true)
-
+      admin(script, true)
       result = script.run(:session_id => 0, :password => 'bluh')
       expect(result[:success?]).to eq false
       expect(result[:error]).to eq :invalid_username
     end
 
     it "requires a username to be at least three characters" do
-      script = DoubleDog::CreateAccount
-      expect(script).to receive(:admin_session?).and_return(true)
-
+      admin(script, true)
       result = script.run(:session_id => 0, :username => 'ab', :password => 'bluh')
       expect(result[:success?]).to eq false
       expect(result[:error]).to eq :invalid_username
     end
 
     it "requires a password" do
-      script = DoubleDog::CreateAccount
-      expect(script).to receive(:admin_session?).and_return(true)
-
+      admin(script, true)
       result = script.run(:session_id => 0, :username => 'bob')
       expect(result[:success?]).to eq false
       expect(result[:error]).to eq :invalid_password
     end
 
     it "requires a password with at least three characters" do
-      script = DoubleDog::CreateAccount
-      expect(script).to receive(:admin_session?).and_return(true)
-
+      admin(script, true)
       result = script.run(:session_id => 0, :username => 'bob', :password => '12')
       expect(result[:success?]).to eq false
       expect(result[:error]).to eq :invalid_password
@@ -54,9 +41,7 @@ describe DoubleDog::CreateAccount do
   end
 
   it "creates an account" do
-    script = DoubleDog::CreateAccount
-    expect(script).to receive(:admin_session?).and_return(true)
-
+    admin(script, true)
     result = script.run(:session_id => 0, :username => 'bob', :password => 'letmein')
     expect(result[:success?]).to eq true
 
--- spec/scripts/create_item_spec.rb
+++ spec/scripts/create_item_spec.rb
@@ -1,48 +1,39 @@
 require 'spec_helper'
 
 describe DoubleDog::CreateItem do
+  let(:script) {DoubleDog::CreateItem}
 
   describe 'Validation' do
     it "requires the user to be an admin" do
-      script = DoubleDog::CreateItem
-      expect(script).to receive(:admin_session?).and_return(false)
-
+      admin(script, false)
       result = script.run(:name => "doesn't matter", :price => 5)
       expect(result[:success?]).to eq false
       expect(result[:error]).to eq :not_admin
     end
 
     it "requires a name" do
-      script = DoubleDog::CreateItem
-      expect(script).to receive(:admin_session?).and_return(true)
-
+      admin(script, true)
       result = script.run(:name => nil, :price => 5)
       expect(result[:success?]).to eq false
       expect(result[:error]).to eq :invalid_name
     end
 
     it "requires the name to be at least one character" do
-      script = DoubleDog::CreateItem
-      expect(script).to receive(:admin_session?).and_return(true)
-
+      admin(script, true)
       result = script.run(:name => '', :price => 5)
       expect(result[:success?]).to eq false
       expect(result[:error]).to eq :invalid_name
     end
 
     it "requires a price" do
-      script = DoubleDog::CreateItem
-      expect(script).to receive(:admin_session?).and_return(true)
-
+      admin(script, true)
       result = script.run(:name => 'x', :price => nil)
       expect(result[:success?]).to eq false
       expect(result[:error]).to eq :invalid_price
     end
 
     it "requires a price to be more than fiftey cents" do
-      script = DoubleDog::CreateItem
-      expect(script).to receive(:admin_session?).and_return(true)
-
+      admin(script, true)
       result = script.run(:name => 'y', :price => 0.4)
       expect(result[:success?]).to eq false
       expect(result[:error]).to eq :invalid_price
@@ -50,9 +41,7 @@ describe DoubleDog::CreateItem do
   end
 
   it "creates an item" do
-    script = DoubleDog::CreateItem
-    expect(script).to receive(:admin_session?).and_return(true)
-
+    admin(script, true)
     result = script.run(:name => 'smoothie', :price => 10)
     expect(result[:success?]).to eq true
 
